4416078b881edbea1c7541242f95ed979d3f00be
Anand-Ochir <anand-ochir@Anand-Ochir.local>
2020-08-07 16:25:41 +0800

    feat router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './style/main.scss'
 
 function App() {
   return (
-    <>
-    </>
+    <Router>
+      <Switch>
+        
+      </Switch>
+    </Router>
   );
 }
 
